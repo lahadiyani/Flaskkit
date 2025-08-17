b5a60ce92cb3a2ab260300d2122f7a0b8d22b281
lahadiyani <lahadiyani@gmail.com>
2025-08-17 11:08:28 +0800

    chore: perbarui metadata di setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="flaskkits-boilerplate",
     version="0.1.0",
+    summary="Starter kit Flask modern dengan struktur rapi, integrasi Tailwind CSS, dan perintah CLI sederhana.",
+    home_page="https://github.com/lahadiyani/flaskkit",
+    author="Hadiani",
+    author_email="lahadiyani@gmail.com",
+    license="MIT",
+    
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
