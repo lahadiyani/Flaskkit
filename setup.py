923485c3660dfa40170bd20c9999e0da6080e59b
lahadiyani <lahadiyani@gmail.com>
2025-08-18 12:23:20 +0800

    fix: subprocess error

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="flaskkits-boilerplate",
+    name="flaskkits_boilerplate",
     version="0.1.0",
-    summary="Starter kit Flask modern dengan struktur rapi, integrasi Tailwind CSS, dan perintah CLI sederhana.",
-    home_page="https://github.com/lahadiyani/flaskkit",
+    description="Starter kit Flask modern dengan struktur rapi, integrasi Tailwind CSS, dan perintah CLI sederhana.",
+    url="https://github.com/lahadiyani/flaskkit",
     author="Hadiani",
     author_email="lahadiyani@gmail.com",
     license="MIT",
-    
+
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -19,7 +19,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "flaskkit=flaskkit.cli:main",
+            "flaskkit=flaskkits_boilerplate.cli:main",
         ],
     },
 )
